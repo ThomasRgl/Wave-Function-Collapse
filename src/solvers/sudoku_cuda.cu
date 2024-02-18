2fd9460437ea6c45d0036a36be3ebe5906e37b72
ThomasRgl <thomasroglin@free.fr>
2024-02-18 17:21:33 +0100

    new clone system

--- src/solvers/sudoku_cuda.cu
+++ src/solvers/sudoku_cuda.cu
@@ -4,6 +4,7 @@
 #include "wfc.cuh"
 #include "utils.cuh"
 
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -47,9 +48,9 @@ solve_cuda_device(wfc_blocks_ptr blocks, wfc_blocks_ptr init, uint64_t seed)
         //     printf("\n======== iteration : %lu ========\n\n", iteration);
         // }
 
-        vec4 loc;
+        vec4 loc = grd_min_entropy(blocks);
         
-        loc = grd_min_entropy(blocks);
+
       
         if( loc.x == UINT8_MAX ){
             success = true;
@@ -57,12 +58,14 @@ solve_cuda_device(wfc_blocks_ptr blocks, wfc_blocks_ptr init, uint64_t seed)
             break;
         }
 
+
         uint64_t * state = blk_at(blocks, loc.gx, loc.gy,
                                       loc.x, loc.y);
         
         if( state == 0){
             break;
         }
+
 
         // if( state == 0 && threadIdx.x == 0 && threadIdx.y == 0) {
         //     printf("state = 0\n");
@@ -80,6 +83,7 @@ solve_cuda_device(wfc_blocks_ptr blocks, wfc_blocks_ptr init, uint64_t seed)
             // printBinary2(collapsed_state);
             // printf(") at : [%u, %u] [%u, %u]\n", loc.gy, loc.gx, loc.y, loc.x);
         }
+
 
         bool error = grd_propagate_all(blocks, loc.gx,
                             loc.gy, loc.x, loc.y, collapsed_state);
@@ -105,8 +109,11 @@ solve_cuda_device(wfc_blocks_ptr blocks, wfc_blocks_ptr init, uint64_t seed)
         grd_print(NULL, blocks);
         blocks->solved = success; 
     }
+    blocks->seed = 999;
+
     // getchar();
     return ;
+
 }
 
 bool
@@ -127,15 +134,18 @@ solve_cuda(wfc_blocks_ptr blocks, wfc_blocks_ptr d_init, uint64_t seed)
 
     // wfc_blocks_ptr b = (wfc_blocks*) malloc(sizeof(wfc_blocks));
 
-    wfc_blocks_ptr b = super_safe_malloc( 3, 3);
-    wfc_blocks_ptr buffer = super_safe_malloc( 3, 3);
-    cudaMemcpy(buffer, blocks, sizeof(wfc_blocks), cudaMemcpyDeviceToHost);
-    cudaMemcpy(b->states, buffer->states, 3 * 3 * 3 * 3 * sizeof(uint64_t), cudaMemcpyDeviceToHost);
-    printf("hello world from Host, success ? : %u \n", b->solved);
-    b->grid_side = 3;
-    b->block_side = 3;
-    grd_print(NULL, b);
-    bool succes = b->solved;
+    // wfc_blocks_ptr b = super_safe_malloc( 3, 3);
+    // wfc_blocks_ptr buffer = super_safe_malloc( 3, 3);
+    uint64_t mseed = 0; 
+    cudaMemcpy(&mseed, &(blocks->seed), sizeof(uint64_t), cudaMemcpyDeviceToHost);
+    checkCudaErrors(cudaGetLastError());
+
+    // cudaMemcpy(b->states, buffer->states, 3 * 3 * 3 * 3 * sizeof(uint64_t), cudaMemcpyDeviceToHost);
+    printf("hello world from Host, success ? seed : %lu \n", mseed);  
+    // b->grid_side = 3;
+    // b->block_side = 3;
+    // grd_print(NULL, b);
+    bool succes = true;
     // super_safe_free(b);
     // super_safe_free(buffer);
     return succes;
